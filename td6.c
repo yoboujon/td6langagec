397dbaddb286def499ca1f5a063d624d3cedcb9a
yoboujon <yoboujon@gmail.com>
2022-11-25 13:40:01 +0100

    Question 7

--- td6.c
+++ td6.c
@@ -3,16 +3,28 @@
 #define SIZECOL 3
 #define SIZELINE 2
 
+int ** Init(int sizeCol,int sizeLine);
 int Affiche2(int ** matrix, int sizeCol, int sizeLine);
 int Saisie2(int ** matrix, int sizeCol, int sizeLine);
 int Saisie2p(int ** matrix, int sizeCol, int sizeLine);
 
 int main(void)
 {
-    int ** line = (int **)(malloc(SIZELINE*sizeof(int *)));
+    int ** line=Init(SIZECOL,SIZELINE);
     Saisie2p(line,SIZECOL,SIZELINE);
     Affiche2(line,SIZECOL,SIZELINE);
     return 0;
+}
+
+int ** Init(int sizeCol,int sizeLine)
+{
+    int ** matrix;
+    matrix = (int **)(malloc(sizeLine*sizeof(int *)));
+    for(int i=0;i<sizeLine;i++)
+    {
+        *(matrix+i) = (int *)(malloc(sizeCol*sizeof(int)));
+    }
+    return matrix;
 }
 
 int Affiche2(int ** matrix, int sizeCol, int sizeLine)
@@ -32,13 +44,11 @@ int Saisie2(int ** matrix, int sizeCol, int sizeLine)
 {
     for(int i=0; i<sizeLine ; i++)
     {
-        int * col = (int *)(malloc(SIZECOL*sizeof(int)));
         for(int j=0; j<sizeCol ; j++)
         {
             printf("matrix[%d][%d] = ",i,j);
-            scanf("%d",&col[j]);
+            scanf("%d",&matrix[i][j]);
         }
-        matrix[i] = col;
     }
     return 0;
 }
@@ -47,13 +57,11 @@ int Saisie2p(int ** matrix, int sizeCol, int sizeLine)
 {
     for(int i=0; i<sizeLine ; i++)
     {
-        int * col = (int *)(malloc(SIZECOL*sizeof(int)));
         for(int j=0; j<sizeCol ; j++)
         {
             printf("matrix[%d][%d] = ",i,j);
-            scanf("%d",&(*(col +j)));
+            scanf("%d",&(*((*(matrix+i))+j)));
         }
-        *(matrix + i) = col;
     }
     return 0;
 }
