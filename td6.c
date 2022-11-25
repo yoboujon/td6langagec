eb2cdfa2f7215bfb4b8cf2aa3ee18c0afd4956a3
yoboujon <yoboujon@gmail.com>
2022-11-25 13:05:32 +0100

    Question 4&5&6

--- td6.c
+++ td6.c
@@ -3,18 +3,19 @@
 #define SIZECOL 3
 #define SIZELINE 2
 
-int print2Dtab(int ** matrix, int sizeCol, int sizeLine);
-int Saisie2(int ** matrix, int colNumber, int sizeLine);
+int Affiche2(int ** matrix, int sizeCol, int sizeLine);
+int Saisie2(int ** matrix, int sizeCol, int sizeLine);
+int Saisie2p(int ** matrix, int sizeCol, int sizeLine);
 
 int main(void)
 {
     int ** line = (int **)(malloc(SIZELINE*sizeof(int *)));
-    Saisie2(line,SIZECOL,SIZELINE);
-    print2Dtab(line,SIZECOL,SIZELINE);
+    Saisie2p(line,SIZECOL,SIZELINE);
+    Affiche2(line,SIZECOL,SIZELINE);
     return 0;
 }
 
-int print2Dtab(int ** matrix, int sizeCol, int sizeLine)
+int Affiche2(int ** matrix, int sizeCol, int sizeLine)
 {
     for(int i=0;i<sizeLine;i++)
     {
@@ -38,6 +39,21 @@ int Saisie2(int ** matrix, int sizeCol, int sizeLine)
             scanf("%d",&col[j]);
         }
         matrix[i] = col;
+    }
+    return 0;
+}
+
+int Saisie2p(int ** matrix, int sizeCol, int sizeLine)
+{
+    for(int i=0; i<sizeLine ; i++)
+    {
+        int * col = (int *)(malloc(SIZECOL*sizeof(int)));
+        for(int j=0; j<sizeCol ; j++)
+        {
+            printf("matrix[%d][%d] = ",i,j);
+            scanf("%d",&(*(col +j)));
+        }
+        *(matrix + i) = col;
     }
     return 0;
 }
